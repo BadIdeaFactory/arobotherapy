dd909d1acfbda0e63058e95495465f2ad61f5216
Dan Schultz <slifty@gmail.com>
2018-10-09 15:13:09 -0400

    Initial project setup
    This contains the default code base generated with the single view
    template in xcode.  Core data and UI tests were NOT selected, unit tests
    were selected.
    
    This project is set up to use Swift.

---
+++ Arobotherapy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arobotherapy
+//
+//  Created by Dan Schultz on 10/9/18.
+//  Copyright © 2018 Bad Idea Factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
