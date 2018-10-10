1e44d744784cdba6af1fd521b8ab6d86c9abd22d
Dan Schultz <slifty@gmail.com>
2018-10-10 11:45:32 -0400

    Add passage reading view skeleton
    We have a window to render text, we have a button to go to the next step
    or to go back to the instructions.
    
    Issue #5 Passage reading screen

---
+++ Arobotherapy/PassageRecordingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PassageRecordingViewController.swift
+//  Arobotherapy
+//
+//  Created by Dan Schultz on 10/10/18.
+//  Copyright © 2018 Bad Idea Factory. All rights reserved.
+//
+
+import UIKit
+
+class PassageRecordingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
